d9455cab00e9b3ff0e3c0a5119e897217f35fd63
Anna Anuoluwapo Ajayi <aaaj48595@edu.ucl.dk>
2023-03-14 10:23:40 +0100

    Opdatere models

---
+++ BudwegErrorLoggingSystem/BudwegErrorLoggingSystem/Models/ErrorReport.cs
@@ -0,0 +1,32 @@
+﻿using BudwegErrorLoggingSystem.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudwegErrorLoggingSystem.Models
+{
+    public class ErrorReport
+    {
+        public int ReportId { get; set; }
+
+        public string ErrorType { get; set; }
+
+        public string ErrorDescription { get; set; }
+
+        public int WorkerId { get; set; }
+
+        public int InspectorId { get; set; }
+        public ErrorReport() { }
+
+        public ErrorReport(int reportId, string errorType, string errorDescription, int workerId, int inspectorId)
+        {
+            ReportId = reportId;
+            ErrorType = errorType;
+            ErrorDescription = errorDescription;
+            WorkerId = workerId;
+            InspectorId = inspectorId;
+        }
+    }
+}
